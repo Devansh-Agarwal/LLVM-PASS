666b7db6572a9b3b06b0fbc814c002dad47e993b
Devansh Agarwal <thedevanshagarwal@gmail.com>
2018-09-24 18:57:40 +0530

    iterates over the basic block

---
+++ itrBB/itrBB.cpp
@@ -0,0 +1,29 @@
+#include "llvm/IR/Function.h"
+#include  "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace
+{
+
+  struct itrBB: public FunctionPass
+  {
+      static char ID;
+      itrBB():FunctionPass(ID){}
+
+      bool runOnFunction(Function &F) override{
+        errs() << "Hello: ";
+        errs() << F.getName() << "\n";
+        for(Function::iterator bb = F.begin(), e = F.end(); bb!= e; bb++)
+        {
+          errs() << "BasicBlock name = " << bb->getName() << "\n";
+          errs() << "BasicBlock size = " << bb->size() << "\n";
+        }
+      }
+  };
+
+}
+
+char itrBB::ID = 0;
+static RegisterPass<itrBB> X("itrBB","iterate over basic block inside a function");
